64a9350b3b0274e7e083ace9633adda065c606a9
Kamil T <trzaskomakamil@gmail.com>
2019-09-16 20:41:41 +0200

    formating changes

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import Dice from "./Dice";
-import ScoreTable from "./ScoreTable";
-import "./Game.css";
+import React, { Component } from 'react'
+import Dice from './Dice'
+import ScoreTable from './ScoreTable'
+import './Game.css'
 
-const NUM_DICE = 5;
-const NUM_ROLLS = 3;
+const NUM_DICE = 5
+const NUM_ROLLS = 3
 
 class Game extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       dice: Array.from({ length: NUM_DICE }),
       locked: Array(NUM_DICE).fill(false),
@@ -28,9 +28,9 @@ class Game extends Component {
         yahtzee: undefined,
         chance: undefined
       }
-    };
-    this.roll = this.roll.bind(this);
-    this.doScore = this.doScore.bind(this);
+    }
+    this.roll = this.roll.bind(this)
+    this.doScore = this.doScore.bind(this)
   }
 
   roll(evt) {
@@ -41,7 +41,7 @@ class Game extends Component {
       ),
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
       rollsLeft: st.rollsLeft - 1
-    }));
+    }))
   }
 
   toggleLocked(idx) {
@@ -52,7 +52,7 @@ class Game extends Component {
         !st.locked[idx],
         ...st.locked.slice(idx + 1)
       ]
-    }));
+    }))
   }
 
   doScore(rulename, ruleFn) {
@@ -61,8 +61,8 @@ class Game extends Component {
       scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
       rollsLeft: NUM_ROLLS,
       locked: Array(NUM_DICE).fill(false)
-    }));
-    this.roll();
+    }))
+    this.roll()
   }
 
   render() {
@@ -81,8 +81,7 @@ class Game extends Component {
               <button
                 className='Game-reroll'
                 disabled={this.state.locked.every(x => x)}
-                onClick={this.roll}
-              >
+                onClick={this.roll}>
                 {this.state.rollsLeft} Rerolls Left
               </button>
             </div>
@@ -90,8 +89,8 @@ class Game extends Component {
         </header>
         <ScoreTable doScore={this.doScore} scores={this.state.scores} />
       </div>
-    );
+    )
   }
 }
 
-export default Game;
+export default Game
