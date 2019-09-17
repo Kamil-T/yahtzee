ac9d1fc3d285856406b84ed83503ff337ba6881f
Kamil T <trzaskomakamil@gmail.com>
2019-09-17 20:45:23 +0200

    add points descriptions and dice icons

--- src/components/RuleRow.js
+++ src/components/RuleRow.js
@@ -3,14 +3,14 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
-    const { score, name, doScore } = this.props
+    const { score, name, description, doScore } = this.props
     let disabled = score != undefined
     return (
       <tr
         className={`RuleRow RuleRow-${disabled ? 'disabled' : 'active'}`}
         onClick={disabled ? null : doScore}>
         <td className='RuleRow-name'>{name}</td>
-        <td className='RuleRow-score'>{score}</td>
+        <td className='RuleRow-score'>{disabled ? score : description}</td>
       </tr>
     )
   }
